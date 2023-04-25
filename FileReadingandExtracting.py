3cba1ca00d50e72f2d9c0ed507f2ababe77d2545
MG Dellomos <dellomosmarygrace0402@gmail.com>
2023-04-25 12:25:46 +0700

    three txt files Creating, Opening, and Reading (DONE)

--- FileReadingandExtracting.py
+++ FileReadingandExtracting.py
@@ -4,7 +4,10 @@
 #The second text file will be named odd.txt that will contains all odd numbers extracted from the numbers.txt.  
 
 #open files named: numbers.txt file (read), even.txt file (append), odd.txt file (append)
-    #read the numbers.txt file line by line    
+with open("numbers.txt") as main_file, open("even.txt", "a") as extracted_even_nums,  open("odd.txt", "a") as extracted_odd_nums:
+    #read the numbers.txt file line by line
+    for line in main_file:
+        print(line.strip())
 #check each lines of numbers.txt
 #IF the line has even number,
     #THEN extract this number and append/write to even.txt file

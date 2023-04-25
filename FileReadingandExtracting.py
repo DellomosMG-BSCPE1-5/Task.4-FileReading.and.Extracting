78d43119c97804a7d2a0d5174fbbf721da42a14c
MG Dellomos <dellomosmarygrace0402@gmail.com>
2023-04-25 12:42:10 +0700

    Even number extraction then appends/writes to even.txt file

--- FileReadingandExtracting.py
+++ FileReadingandExtracting.py
@@ -7,9 +7,14 @@
 with open("numbers.txt") as main_file, open("even.txt", "a") as extracted_even_nums,  open("odd.txt", "a") as extracted_odd_nums:
     #read the numbers.txt file line by line
     for line in main_file:
-        print(line.strip())
-#check each lines of numbers.txt
-#IF the line has even number,
-    #THEN extract this number and append/write to even.txt file
-#ELSE, if the line has odd number,
-    #THEN extract this number and append/write to odd.txt file
+        main_file_nums = int(line)
+        #check each lines of numbers.txt
+        #IF the line has even number,
+        if main_file_nums % 2 == 0:
+            #THEN extract this number and append/write to even.txt file
+            extracted_even_nums.write(str(main_file_nums) + "\n")
+            print(extracted_even_nums)
+        #ELSE, if the line has odd number,
+        else:
+            #THEN extract this number and append/write to odd.txt file
+            print()

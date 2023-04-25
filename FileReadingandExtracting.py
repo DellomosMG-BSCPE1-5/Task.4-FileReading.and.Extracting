9eb3497e13ca7d42679b47d39f68f591e9e2b710
MG Dellomos <dellomosmarygrace0402@gmail.com>
2023-04-25 12:07:04 +0700

    message

---
+++ FileReadingandExtracting.py
@@ -0,0 +1,12 @@
+#Write a Python program that reads a text file named numbers.txt that contains 20 integers. 
+#The program will create two other text file; 
+#The first text file will be named even.txt that will contains all even numbers extracted from the numbers.txt. 
+#The second text file will be named odd.txt that will contains all odd numbers extracted from the numbers.txt.  
+
+#open files named: numbers.txt file (read), even.txt file (append), odd.txt file (append)
+    #read the numbers.txt file line by line    
+#check each lines of numbers.txt
+#IF the line has even number,
+    #THEN extract this number and append/write to even.txt file
+#ELSE, if the line has odd number,
+    #THEN extract this number and append/write to odd.txt file
